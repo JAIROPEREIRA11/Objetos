1cb403eef9970376a524e86722b06b0de0244cea
JAIROPEREIRA11 <jairo.pereira.silva@escola.pr.gov.br>
2026-09-03 12:23:21 -0300

    funcao

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "11444444440"],
+    saldo: 200,
+    efetuaPagamento: function (valor) {
+        if (valor > this.saldo) {
+            console.log("Saldo Insuficiente");
+        } 
+    }
+};
